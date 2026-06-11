7341f0204e924409ae385898d272b8e4cdf0d208
pipe343123 <pipe343123702@gmail.com>
2026-06-11 10:12:38 -0500

    fix: almacenamiento estatico optimizado.

--- django_tejobar/tejobar_django/settings.py
+++ django_tejobar/tejobar_django/settings.py
@@ -111,9 +111,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Esto evita que el despliegue falle si hay alguna referencia rota en el CSS/Bootstrap
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 MEDIA_URL = "/media/"
